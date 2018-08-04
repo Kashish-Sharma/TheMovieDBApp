ecc4c9f83d01b732809efc6ced92614aa93c9b7c
Kashish Sharma <kashish25798@gmail.com>
2018-08-04 23:39:14 +0530

    Add: ReviewReadMore Bottom Sheet

---
+++ app/src/main/java/kashish/com/interfaces/OnReviewReadMoreClickListener.kt
@@ -0,0 +1,9 @@
+package kashish.com.interfaces
+import kashish.com.models.MovieReview
+
+/**
+ * Created by Kashish on 04-08-2018.
+ */
+interface OnReviewReadMoreClickListener {
+    fun onReviewReadMoreClickListener(review: MovieReview)
+}
--- app/src/main/java/kashish/com/viewholders/ReviewViewHolder.kt
+++ app/src/main/java/kashish/com/viewholders/ReviewViewHolder.kt
@@ -6,12 +6,16 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kashish.com.R
+import kashish.com.interfaces.OnReviewReadMoreClickListener
+import kashish.com.models.MovieReview
 import kotlinx.android.synthetic.main.review_single_item.view.*
 
 /**
  * Created by Kashish on 02-08-2018.
  */
-class ReviewViewHolder(itemView: View?, context: Context) : RecyclerView.ViewHolder(itemView) {
+class ReviewViewHolder(itemView: View?, context: Context,
+                       val reviewList: List<MovieReview>,
+                       val listener: OnReviewReadMoreClickListener) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
     var mReviewAuthor: TextView
     var mReviewContent: TextView
@@ -21,6 +25,16 @@ class ReviewViewHolder(itemView: View?, context: Context) : RecyclerView.ViewHol
         mReviewAuthor = itemView!!.findViewById(R.id.review_author_name)
         mReviewContent = itemView.findViewById(R.id.review_content)
         mReadMoreButton = itemView.findViewById(R.id.review_read_more)
+
+        itemView.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+        val position: Int = adapterPosition
+        if (position!=RecyclerView.NO_POSITION){
+            listener.onReviewReadMoreClickListener(reviewList.get(position))
+        }
     }
 
 }
