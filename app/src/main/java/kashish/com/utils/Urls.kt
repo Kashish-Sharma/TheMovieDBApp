d3327b373a53b4dd60d93d2a6b0c6b1ea153dd20
Kashish Sharma <kashish25798@gmail.com>
2018-09-30 11:33:37 +0530

    Add: Urls.kt

---
+++ app/src/main/java/kashish/com/utils/Urls.kt
@@ -0,0 +1,13 @@
+package kashish.com.utils
+
+
+/**
+ * Created by Kashish on 31-07-2018.
+ */
+class Urls {
+    companion object {
+
+        val BASE_URL = "http://api.themoviedb.org/3/"
+        val MOVIE_DETAILS_BASE_URL = "https://api.themoviedb.org/3/movie/"
+    }
+}
